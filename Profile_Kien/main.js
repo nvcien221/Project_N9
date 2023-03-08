53f18a71ddc78df045615bc130e20afa00725fe0
nvcien221 <119002377+nvcien221@users.noreply.github.com>
2023-03-08 23:37:46 +0700

    Add files via upload

---
+++ Profile_Kien/main.js
@@ -0,0 +1,9 @@
+function animate_string() {
+    const element = document.getElementById('target');
+    const textNode = element.firstChild;
+    let text = textNode.data;
+    setInterval(() => {
+        text = text[text.length -1] + text.substring(0, text.length - 1);
+        textNode.data = text;
+    }, 200);
+}
