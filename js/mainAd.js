991fdeaf72764b08b900348b38155a5d2f0a3889
Cien <vnk22012002@gmail.com>
2023-05-16 07:44:12 +0700

    Update

---
+++ js/mainAd.js
@@ -0,0 +1,7 @@
+
+document.getElementById("showOverlay").addEventListener("click", function () {
+    document.getElementById("search").classList.add("show");
+})
+document.getElementById("closeOverlay").addEventListener("click", function () {
+    document.getElementById("search").classList.remove("show");
+})
